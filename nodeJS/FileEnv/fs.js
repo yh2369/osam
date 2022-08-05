468c4d12cd81ae1fc5719e3f5c8952b710571716
Guest <guest-eirodr@h27247.sgb.skir.kr>
2022-08-05 23:45:16 +0900

    Env

--- nodeJS/FileEnv/fs.js
+++ nodeJS/FileEnv/fs.js
@@ -1,12 +1,11 @@
+
+/*
 const fs = require("fs");
-
 const content = "Hello World";
-
 fs.writeFile("./hello.txt", content, (err)=>{
     if(err){
         console.error(err);
     }
-
     fs.readFile("./hello.txt", "utf-8", (err, data) =>{
         if(err){
             console.error(err);
@@ -16,3 +15,24 @@ fs.writeFile("./hello.txt", content, (err)=>{
         console.log(data);
     });
 });
+*/
+
+const fs = require("fs/promises");
+
+const content = "Hello World";
+
+async function helloWorld()
+{
+    try {
+        await fs.writeFile("process.env.FILE_PATH", content);
+        const data = await fs.readFile(process.env.FILE_PATH, {encoding: "utf-8"});
+        console.log(data);
+    }
+    catch(err)
+    {
+        console.error(err);
+    }
+
+}
+
+helloWorld();
