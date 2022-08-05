2f5d63c0d1f8e34c49c2feb453a6bfd6b81f5c2b
Guest <guest-eirodr@h27247.sgb.skir.kr>
2022-08-05 22:45:49 +0900

    fs

--- nodeJS/Module/hello.js
+++ nodeJS/Module/hello.js
@@ -1,3 +1,8 @@
 const {HelloWorld} = require("./function.js");
-
 HelloWorld();
+
+const fs = require("fs");
+const helloFile = fs.readFileSync("./hello.json").toString();
+const helloJson = JSON.parse(helloFile);
+
+console.log(helloJson.contents);
